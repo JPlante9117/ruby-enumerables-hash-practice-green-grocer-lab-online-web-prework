1b88f5296fd87e8c9347ce28462846ab383e04eb
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 14:57:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,7 +25,7 @@ def apply_coupons(cart, coupons)
       cart[item][:count] -= coupon[:num]                                        
                                               #then create the discounted item and it's hash. coupon[:num] gives you the count for the coupons. Therefore, you can decrease the non-discounted items by the number of coupons and create a coupon set that IS discounted
                                               
-      elsif cart[item] && cart[item][:count] >= coupon[:num] && cart.has_key?(appliedcoupon)
+      elsif cart[item][:count] >= coupon[:num] && cart.has_key?(appliedcoupon)
       
                                               #Otherwise, if there IS a cart[item w/ coupon] in existence . . .
                                               
