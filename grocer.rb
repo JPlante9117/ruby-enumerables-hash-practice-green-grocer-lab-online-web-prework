b603af66af59f4a1d0a9843ce90054d3fb2f38f2
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 14:30:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,7 +16,7 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon[:item]                      #selects the item that the coupon applies to via the hash given
     
-    if cart[:item] && 
+    if cart[item] && coupon[:item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
       
     
   end
